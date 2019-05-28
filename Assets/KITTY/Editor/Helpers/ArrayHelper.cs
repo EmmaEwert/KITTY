1473414e3b48dfc00d6864fecc15a07238ce0f79
Emma Ewert <EmmaEwert@users.noreply.github.com>
2019-05-29 01:50:20 +0200

    Improve ArrayHelper documentation

--- Assets/KITTY/Editor/Helpers/ArrayHelper.cs
+++ Assets/KITTY/Editor/Helpers/ArrayHelper.cs
@@ -1,27 +1,36 @@
 ﻿namespace KITTY {
-	///<summary>Miscellaneous `Array` helper methods</summary>
+	///<summary>
+	///Miscellaneous `Array` helper methods
+	///</summary>
 	internal static class ArrayHelper {
-		///<summary>Reverse the row order in a 2D block of elements, assuming width `stride`.</summary>
+		///<summary>
+		///Reverse the row order in a 2D block of elements, stored in a 1D array, assuming a width
+		///of `stride`.
+		///</summary>
 		public static T[] Reverse<T>(T[] input, int stride) {
 			var size = input.Length / stride;
 			var output = new T[input.Length];
 			for (var row = 0; row < size; ++row) {
-				for (var col = 0; col < stride; ++col) {
-					var i = row * stride + col;
-					var j = (size - row - 1) * stride + col;
+				for (var column = 0; column < stride; ++column) {
+					var i = row * stride + column;
+					var j = (size - row - 1) * stride + column;
 					output[i] = input[j];
 				}
 			}
 			return output;
 		}
 
+		///<summary>
+		///Flip rows and columns in a 2D block of elements, stored in a 1D array, assuming a width
+		///of `stride`.
+		///</summary>
 		public static T[] Swizzle<T>(T[] input, int stride) {
 			var size = input.Length / stride;
 			var output = new T[input.Length];
 			for (var row = 0; row < size; ++row) {
-				for (var col = 0; col < stride; ++col) {
-					var i = col * stride + row;
-					var j = row * stride + col;
+				for (var column = 0; column < stride; ++column) {
+					var i = column * stride + row;
+					var j = row * stride + column;
 					output[i] = input[j];
 				}
 			}
