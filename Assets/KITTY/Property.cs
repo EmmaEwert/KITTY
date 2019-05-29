b9b5a92f61689bf06853078d10ecbe443b005ac6
Emma Ewert <EmmaEwert@users.noreply.github.com>
2019-05-29 03:32:52 +0200

    Improve Property and Tile documentation

--- Assets/KITTY/Property.cs
+++ Assets/KITTY/Property.cs
@@ -5,12 +5,18 @@ namespace KITTY {
 	using System.Reflection;
 	using UnityEngine;
 
+	///<summary>
+	///Direct C# representation of a Tiled custom property.
+	///</summary>
 	[Serializable]
 	public struct Property {
 		public string name;
 		public string type;
 		public string value;
 
+		///<summary>
+		///Merges a primary and a secondary array of properties, favoring the primary array.
+		///</summary>
 		public static Property[] Merge(Property[] primary, Property[] secondary) {
 			var properties = new List<Property>();
 			for (var i = 0; i < secondary?.Length; ++i) {
@@ -22,11 +28,15 @@ namespace KITTY {
 			return properties.ToArray();
 		}
 
+		///<summary>
+		///Applies an array of properties to a component's fields if they're decorated with
+		///TiledPropertyAttribute and match a property name.
+		///</summary>
 		public static void Apply(Property[] properties, MonoBehaviour component) {
 			foreach (var field in component.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)) {
 				var attribute = field.GetCustomAttribute<TiledPropertyAttribute>();
 				if (attribute == null) { continue; }
-				var fieldName = attribute.name ?? field.Name.ToLower();
+				var fieldName = attribute.name?.ToLower() ?? field.Name.ToLower();
 				var fieldType = field.FieldType.ToString();
 				switch (fieldType) {
 					case "System.String":  fieldType = "string"; break;
@@ -35,12 +45,21 @@ namespace KITTY {
 					case "System.Boolean": fieldType = "bool";   break;
 				}
 				foreach (var property in properties) {
-					if (property.name.ToLower().Replace(" ", "") == fieldName && property.type == fieldType) {
+					if (property.name.ToLower().Replace(" ", "") == fieldName
+						&& property.type == fieldType) {
 						switch (property.type) {
-							case "string": field.SetValue(component,             property.value);  break;
-							case "int":    field.SetValue(component,   int.Parse(property.value)); break;
-							case "float":  field.SetValue(component, float.Parse(property.value)); break;
-							case "bool":   field.SetValue(component,  bool.Parse(property.value)); break;
+							case "string":
+								field.SetValue(component, property.value);
+								break;
+							case "int":
+								field.SetValue(component, int.Parse(property.value));
+								break;
+							case "float":
+								field.SetValue(component, float.Parse(property.value));
+								break;
+							case "bool":
+								field.SetValue(component, bool.Parse(property.value));
+								break;
 						}
 					}
 				}
--- Assets/KITTY/Tile.cs
+++ Assets/KITTY/Tile.cs
@@ -5,14 +5,26 @@ namespace KITTY {
 	using UnityEngine;
 	using UnityEngine.Tilemaps;
 
+	///<summary>
+	///Tile asset inheriting from Unity's built-in tile, with support for variable-framerate
+	///animation, as well as external prefab instantiation and property application.
+	///</summary>
 	[Serializable]
 	public class Tile : UnityEngine.Tilemaps.Tile {
 		public Frame[] frames;
 		public Property[] properties;
 		public GameObject prefab;
 
+		///<summary>
+		///Tile animation `data` expects its `animatedSprites` field populated by a list of sprites
+		///to be cycled through at a constant framerate.
+		///</summary>
 		public override bool GetTileAnimationData(Vector3Int position, ITilemap tilemap, ref TileAnimationData data) {
 			if (frames == null || frames.Length == 0) { return false; }
+			// Tiled supports individual frame durations per frame, but Unity doesn't. This is
+			// solved by finding the greatest divisor between all frame durations, using that for
+			// the fixed framerate, and replicating each frame enough times to match its original
+			// duration.
 			var duration = GreatestCommonDivisor(frames.Select(f => f.duration).ToArray());
 			var sprites = new List<Sprite>();
 			for (var i = 0; i < frames.Length; ++i) {
@@ -36,6 +48,9 @@ namespace KITTY {
 
 		int GreatestCommonDivisor(int a, int b) => a == 0 ? b : GreatestCommonDivisor(b % a, a);
 
+		///<summary>
+		///A single animation frame for a tile, defined by its individual sprite and duration.
+		///</summary>
 		[Serializable]
 		public struct Frame {
 			public Sprite sprite;
