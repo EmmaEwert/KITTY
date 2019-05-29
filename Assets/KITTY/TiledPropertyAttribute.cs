c898e8bd3c4057a47bb749cd99d4aa380641a898
Emma Ewert <EmmaEwert@users.noreply.github.com>
2019-05-29 09:31:01 +0200

    Improve TiledPropertyAttribute documentation

--- Assets/KITTY/TiledPropertyAttribute.cs
+++ Assets/KITTY/TiledPropertyAttribute.cs
@@ -1,10 +1,16 @@
 namespace KITTY {
 	using System;
 
+	///<summary>
+	///Attribute indicating that a field should be assigned from a Tiled custom property, if any.
+	///</summary>
 	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Method)]
 	public class TiledPropertyAttribute : Attribute {
 		public string name;
 
+		///<summary>
+		///Assign from Tiled custom property, optionally with a different `name`.
+		///</summary>
 		public TiledPropertyAttribute(string name = null) {
 			this.name = name;
 		}
